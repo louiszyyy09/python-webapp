a55124420004adc4d4fc0bf7775b5e4e5b0328e1
ไอยวรินทร์ แก้วรักษา <auqxst0@MacBook.local>
2026-07-30 18:21:23 +0700

    fix: update base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/python-webapp/', // Ensures relative paths for GitHub Pages compatibility
+  base: '/', // Ensures relative paths for GitHub Pages compatibility
 })
